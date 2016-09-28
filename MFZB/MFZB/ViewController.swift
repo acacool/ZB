5353a3f2dc56dbb414a5b0090cff017d575539f1
acacool <acacool@163.com>
2016-09-28 18:06:27 +0800

    初始化项目

---
+++ MFZB/MFZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MFZB
+//
+//  Created by 费劲 on 16/9/28.
+//  Copyright © 2016年 费劲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
